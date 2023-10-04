40dfb373a5749907b7fb908a973d7947c81d83ef
RickyLambert87 <rickylambert87@mail.com>
2023-10-04 15:46:12 +0100

    modified js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,116 +6,152 @@ var seconds = 0;
 var tens = 0;
 var appendTens = document.getElementById("tens");
 var appendSeconds = document.getElementById("seconds");
-var Interval ;
-var images = [
-  "drinker",
-  "strong",
-  "healthy",
-  "traveller",
-  "peaceful",
-  "happy"
+var Interval;
+var gameTimeout;
+var currentRound = 1;
+var timeLimits = {1: 15000, 2: 10000, 3: 5000 };
+var images = ["drinker", "strong", "healthy", "traveller", "peaceful", "happy"];
+var cards = images.concat(images.slice(0)); // duplicate and merge arrays
 
-];
-var clone = images.slice(0); // duplicate array
-var cards = images.concat(clone); // merge to arrays
-var gameStarted = false; // Variable to track whether the game has started
-
-// Shuffel function
-function shuffle(r){
-  for(var j, x, i = r.length; i; j = Math.floor(Math.random() * i),
-  x = r[--i], r[i] = r[j], r[j] = x);
-  return r;
+function shuffle(r) {
+    for (var j, x, i = r.length; i; j = Math.floor(Math.random() * i), x = r[--i], r[i] = r[j], r[j] = x);
+    return r;
 }
-shuffle(cards);
-for (var i = 0; i < cards.length; i++) {
-  card = document.createElement("div");
-  card.dataset.item = cards[i];
-  card.dataset.view = "card";
-  myCards.appendChild(card);
-  card.onclick = function () {
-    if (this.className !="flipped" && this.className != "correct"){
-        this.className ="flipped";
+
+function cardClickEvent() {
+    if (this.className != "flipped" && this.className != "correct") {
+        this.className = "flipped";
         var result = this.dataset.item;
         resultsArray.push(result);
         clearInterval(Interval);
         Interval = setInterval(startTimer, 10);
     }
     if (resultsArray.length > 1) {
-      if (resultsArray[0] === resultsArray[1]) {
-        check("correct");
-        counter ++;
-        win();
-        resultsArray = [];
-      } else {
-        check("reverse");
-        resultsArray = [];
-      }
+        if (resultsArray[0] === resultsArray[1]) {
+            check("correct");
+            counter++;
+            win();
+            resultsArray = [];
+        } else {
+            check("reverse");
+            resultsArray = [];
+        }
     }
-  }
-};
-
-
-var check = function(className) {
-  var x = document.getElementsByClassName("flipped");
-  setTimeout(function() {
-
-    for(var i = (x.length - 1); i >= 0; i--) {
-      x[i].className = className;
-    }
-  },500);
 }
 
-var win = function () {
-
-  if(counter === 6) {
-    clearInterval(Interval);
-    text.innerHTML = "Your time was " + seconds + ":" + tens;
-}
-    }
-    function startTimer () {
-  tens++;
-  if(tens < 9){
-    appendTens.innerHTML = "0" + tens;
-    }
-  if (tens > 9){
-    appendTens.innerHTML = tens;
-    }
-  if (tens > 99) {
-    seconds++;
-    appendSeconds.innerHTML = "0" + seconds;
-    tens = 0;
-    appendTens.innerHTML = "0" + 0;
-    }
-  if (seconds > 9){
-    appendSeconds.innerHTML = seconds;
-   }
+function check(className) {
+    var x = document.getElementsByClassName("flipped");
+    setTimeout(function () {
+        for (var i = (x.length - 1); i >= 0; i--) {
+            x[i].className = className;
+        }
+    }, 500);
 }
 
-var container = document.getElementById("container");
+function win() {
+    if (counter === 6) {
+        clearInterval(Interval);
+        clearTimeout(gameTimeout);
+        text.innerHTML = "Your time was " + seconds + ":" + tens;
+        if (currentRound < 3) {
+            alert("Moving to next round!");
+            currentRound++;
+            resetGame();
+            startGame();
+        } else {
+            alert("Congratulations! You completed all rounds.");
+            endGame();
+        }
+    }
+}
 
-// audio
-container.addEventListener("click", function () {
-  if (!gameStarted) { // Check if the game has not started yet
-    // Get the audio element
-    var audio = document.getElementById("myAudio");
+function startTimer() {
+    tens++;
+    if (tens <= 9) appendTens.innerHTML = "0" + tens;
+    else if (tens > 9) appendTens.innerHTML = tens;
 
-    // Play the audio
-    audio.play();
+    if (tens > 99) {
+        seconds++;
+        appendSeconds.innerHTML = (seconds > 9) ? seconds : "0" + seconds;
+        tens = 0;
+        appendTens.innerHTML = "0" + 0;
+    }
+}
 
-    // Set gameStarted to true to prevent further plays
-    gameStarted = true;
-
-    // Start the game logic (e.g., card flipping, timer, etc.)
-    startGame();
-  }
-});
-
-// Function to start the game logic (you can replace this with your existing game logic)
 function startGame() {
-  // Your existing game logic here
+    shuffle(cards);
+    gameTimeout = setTimeout(function () {
+        if (counter < 6) {
+            gameOver();
+        }
+    }, timeLimits[currentRound]);
 }
 
-// Reset gameStarted to false when the game ends (You can call this function when the game ends)
-function endGame() {
-  gameStarted = false;
+function gameOver() {
+    alert("Time's up for this round!");
+    resetGame();
+    startGame();
 }
+
+function resetGame() {
+  // Clearing the container
+  myCards.innerHTML = "";
+
+  // Shuffling the cards
+  shuffle(cards);
+
+  // Populating the container with shuffled cards
+  for (var i = 0; i < cards.length; i++) {
+      var card = document.createElement("div");
+      card.dataset.item = cards[i];
+      card.dataset.view = "card";
+      card.onclick = cardClickEvent;
+      myCards.appendChild(card);
+  }
+
+  // Resetting game variables
+  counter = 0;
+  tens = 0;
+  seconds = 0;
+  appendTens.innerHTML = "00";
+  appendSeconds.innerHTML = "00";
+}
+
+// Card click event handler
+function cardClickEvent() {
+  if (this.className != "flipped" && this.className != "correct") {
+      this.className = "flipped";
+      var result = this.dataset.item;
+      resultsArray.push(result);
+      clearInterval(Interval);
+      Interval = setInterval(startTimer, 10);
+  }
+
+  if (resultsArray.length > 1) {
+      if (resultsArray[0] === resultsArray[1]) {
+          check("correct");
+          counter++;
+          win();
+          resultsArray = [];
+      } else {
+          check("reverse");
+          resultsArray = [];
+      }
+  }
+}
+
+function endGame() {
+    currentRound = 1;
+    resetGame();
+}
+
+// Audio play logic
+var gameStarted = false;
+document.getElementById("container").addEventListener("click", function () {
+    if (!gameStarted) {
+        document.getElementById("myAudio").play();
+        gameStarted = true;
+        startGame();
+    }
+});
+resetGame();
