a59ab67147123918ce49bd84553c5acc3ae55eee
RickyLambert87 <rickylambert87@mail.com>
2023-10-04 11:20:25 +0100

    added controls to audio

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,50 +1,45 @@
-var myCards = document.getElementById('container');
+var myCards = document.getElementById("container");
 var resultsArray = [];
 var counter = 0;
-var text = document.getElementById('text');
-var seconds = 00; 
-var tens = 00; 
+var text = document.getElementById("text");
+var seconds = 0;
+var tens = 0;
 var appendTens = document.getElementById("tens");
 var appendSeconds = document.getElementById("seconds");
 var Interval ;
 var images = [
-  'drinker', 
-  'strong', 
-  'healthy', 
-  'traveller', 
-  'peaceful',
-  'happy'
+  "drinker",
+  "strong",
+  "healthy",
+  "traveller",
+  "peaceful",
+  "happy"
 
 ];
 var maxTimeLimitInSeconds = 20;
 var clone = images.slice(0); // duplicate array
-var cards = images.concat(clone); // merge to arrays 
+var cards = images.concat(clone); // merge to arrays
 
 // Shuffel function
-function shuffle(o){
-  for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-  return o;
+function shuffle(r){
+  for(var j, x, i = r.length; i; j = Math.floor(Math.random() * i), x = r[--i], r[i] = r[j], r[j] = x);
+  return r;
 }
 shuffle(cards);
-
 for (var i = 0; i < cards.length; i++) {
-  card = document.createElement('div');
+  card = document.createElement("div");
   card.dataset.item = cards[i];
   card.dataset.view = "card";
   myCards.appendChild(card);
-     
   card.onclick = function () {
-   
-    if (this.className != 'flipped' && this.className != 'correct'){
-        this.className = 'flipped';
+    if (this.className !='flipped' && this.className != 'correct'){
+        this.className ='flipped';
         var result = this.dataset.item;
         resultsArray.push(result);
         clearInterval(Interval);
         Interval = setInterval(startTimer, 10);
     }
-  
     if (resultsArray.length > 1) {
-
       if (resultsArray[0] === resultsArray[1]) {
         check("correct");
         counter ++;
@@ -54,25 +49,19 @@ for (var i = 0; i < cards.length; i++) {
         check("reverse");
         resultsArray = [];
       }
-      
     }
-    
   }
-   
 };
 
 
 var check = function(className) {
-  
   var x = document.getElementsByClassName("flipped");
   setTimeout(function() {
 
     for(var i = (x.length - 1); i >= 0; i--) {
       x[i].className = className;
     }
-     
   },500);
-   
 }
 
 var win = function () {
@@ -80,31 +69,23 @@ var win = function () {
   if(counter === 6) {
     clearInterval(Interval);
     text.innerHTML = "Your time was " + seconds + ":" + tens;
-  } 
-  
 }
-     
-function startTimer () {
-  tens++; 
-    
+    }
+    function startTimer () {
+  tens++;
   if(tens < 9){
     appendTens.innerHTML = "0" + tens;
-  }
-    
+    }
   if (tens > 9){
     appendTens.innerHTML = tens;
-      
-  } 
-    
+    }
   if (tens > 99) {
     seconds++;
     appendSeconds.innerHTML = "0" + seconds;
     tens = 0;
     appendTens.innerHTML = "0" + 0;
-  }
-    
+    }
   if (seconds > 9){
     appendSeconds.innerHTML = seconds;
-  }
-  
+   }
 }
